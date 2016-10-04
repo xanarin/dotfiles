17c67b7417c628b39f4a95b268ea71df2bb5a647
Will Sloan <sloan.848@osu.edu>
2016-10-04 18:57:04 -0400

    Change working dir to .dotfiles and no angular in vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 ########## Variables
  
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
 files=".ackrc .vimrc .bashrc .tmux.conf"        # list of files/folders to symlink in homedir
  
 ##########
