3446402318acd2e9f112dc7b42114ec7e59204e9
Will Sloan <wsloan@cc-sw.com>
2018-04-25 11:51:02 -0400

    Update locker for blackbox

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,5 +3,6 @@ param=("--textcolor=00000000" "--insidecolor=0000001c" "--ringcolor=0000003e" \
         "--linecolor=00000000" "--keyhlcolor=ffffff80" "--ringvercolor=ffffff00" \
         "--separatorcolor=22222260" "--insidevercolor=ffffff1c" \
         "--ringwrongcolor=ffffff55" "--insidewrongcolor=ffffff1c")
+# Disabled for 18 hour sorex streaming test
 mpc pause
 i3lock -i $HOME/.scripts/current_wallpaper "${param[@]}"
