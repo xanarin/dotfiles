460359d5d4e92532131a6e10932d001ac64760b5
Will Sloan <wsloan@cc-sw.com>
2018-05-22 17:04:00 -0400

    Change notifications for Spotify, Matrix, and Mattermost
    The sound of the notifications was the principle focus of these changes.

--- scripts/beep.sh
+++ scripts/beep.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-/usr/bin/mpv --really-quiet --volume 100 /home/will/.scripts/notification &
+if [ "$5" == "LOW" ]; then
+    exit 0;
+fi
+
+if [ "$1" == "Electron" ]; then
+    # Make different noise for Mattermost and Matrix
+    /usr/bin/mpv --really-quiet --volume 100 /home/will/.scripts/confident.ogg &
+else
+    /usr/bin/mpv --really-quiet --volume 100 /home/will/.scripts/facebook.ogg &
+fi
+
+
