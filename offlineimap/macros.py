f1e52dc1013fe880e60f0675877fca78dacfef51
Will Sloan <wsloan@cc-sw.com>
2018-05-22 17:34:58 -0400

    Allow OfflineIMAP to show messages on user's desktop
    This allows OfflineIMAP (and pass) to tell the user to insert the GPG
    hardware key. Previously, the OfflineIMAP process just failed silently.

--- offlineimap/macros.py
+++ offlineimap/macros.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python2
 import subprocess
+import os
 
 def mailpasswd(account):
-    return subprocess.check_output(["pass", "OfflineIMAP/{}".format(account)]).strip()
+    try:
+        passcode = subprocess.check_output(["pass", "OfflineIMAP/{}".format(account)], env=dict(os.environ, DISPLAY=":0.0")).strip()
+        return passcode
+    except subprocess.CalledProcessError as err:
+        raise Exception("Failed to get GPG key with error:\n{}".format(err.output))
